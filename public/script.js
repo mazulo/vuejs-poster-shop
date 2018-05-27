27bd219441dfe1fb3114313438faddf3169df3c1
Patrick Mazulo <pmazulo@gmail.com>
2018-05-27 00:54:49 -0300

    add the first vue instance to the project

--- public/script.js
+++ public/script.js
@@ -1,1 +1,11 @@
-console.log('It works.');
+new Vue({
+    el: '#app',
+    data: {
+        total: 1900,
+    },
+    methods: {
+        addItem: function() {
+            this.total += 1.599;
+        }
+    }
+});
